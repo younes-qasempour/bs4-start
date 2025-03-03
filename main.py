581426a2ec1f6ac490763f04ec2638c8b4545dcc
younes_qasempour <qasempouryounes@gmail.com>
2025-03-03 12:13:56 +0330

    Learning

--- main.py
+++ main.py
@@ -4,10 +4,32 @@ with open("website.html") as file:
     contents = file.read()
 
 soup = BeautifulSoup(contents, "html.parser")
-print(soup.title)
+# print(soup.title)
+# print(soup.title.name)
+# print(soup.title.string)
+# print(soup.prettify())
+# print(soup.prettify)
 
+# all_anchor_tags = soup.find_all(name="a")
+# print(all_anchor_tags)
+# for tag in  all_anchor_tags:
+    # print(tag.getText())
+    # print(tag.get("href"))
 
+# heading = soup.find_all(name="h1", id="name")
+# print(heading)
 
+# section_heading = soup.find(name="h3", class_="heading")
+# print(section_heading.get("class"))
+
+# company_url = soup.select_one(selector="p a")
+# print(company_url)
+
+# name = soup.select_one(selector="#name")
+# print(name)
+
+# headings = soup.select(selector=".heading")
+# print(headings)
 
 
 
